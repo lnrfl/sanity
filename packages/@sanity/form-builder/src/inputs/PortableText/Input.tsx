2f8f4a2f63fb1e2b2bad1e4581176972b7a9baf8
Bjørge Næss <bjoerge@gmail.com>
2020-09-15 17:56:08 +0200

    Hack focus path checking to make pte connectors work

--- packages/@sanity/form-builder/src/inputs/PortableText/Input.tsx
+++ packages/@sanity/form-builder/src/inputs/PortableText/Input.tsx
@@ -238,7 +238,7 @@ export default function PortableTextInput(props: Props) {
       )
     }
     return (
-      <ChangeIndicatorScope path={[{_key: block._key}]}>
+      <ChangeIndicatorScope path={[{_key: block._key}, 'children']}>
         <ChangeIndicator compareDeep>{returned}</ChangeIndicator>
       </ChangeIndicatorScope>
     )
